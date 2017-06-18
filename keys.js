d0dfa1b7b487ae76dacc6c65db2c54cafe1070b6
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-06-18 00:25:42 -0700

    “init”

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'HrKJGooanZUBZlFru4FbumxWb',
+  consumer_secret: 'nnChxMVNleb8Q6q8Z5nvyjYCQ9SGuLSX6KrkggvIXU96o4OElz',
+  access_token_key: '876286620743245824-FXLC3Gcq6lS018SAW3s4WvDLtyH6NoV',
+  access_token_secret: '	rr9YAOqhBi3A0qJ8pewxsZvLR9ocIuJTVbhkD2TyR51zZ',
+}
