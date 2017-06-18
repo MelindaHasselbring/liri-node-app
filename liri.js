7342406a5d89a82ca2a7fa69efa56c07ad3f9479
J.Hasselbring <janmykhail.hasselbring@outlook.com>
2017-06-18 10:48:32 -0500

    I keep getting authentication errors

--- liri.js
+++ liri.js
@@ -1,6 +1,33 @@
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var request = require('request');
 var keys = require('./keys.js');
 
-console.log(keys.twitterKeys.consumer_key);
+
+switch (process.argv[2]) {
+    case 'my-tweets':
+        var client = new Twitter({
+            consumer_key: keys.twitterKeys.consumer_key,
+            consumer_secret: keys.twitterKeys.consumer_secret,
+            access_token_key: keys.twitterKeys.access_token_key,
+            access_token_secret: keys.twitterKeys.access_token_secret
+        });
+        var params = {screen_name: 'jayhasselbring'};
+
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (error) throw error;
+            console.log(error);  // The errors.
+            console.log(tweets);  // The favorites.
+            console.log(response);  // Raw response object.
+        });
+        break;
+    case 'spotify-this-song ':
+        console.log(spotify);
+        break;
+    case 'movie-this':
+        console.log('Movies');
+        break;
+    case 'do-what-it-says':
+        console.log('Do what it says');
+        break;
+}
