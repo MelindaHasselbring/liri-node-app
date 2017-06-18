39c23c885f4bef9374538e158d876a98b1bbf287
J.Hasselbring <janmykhail.hasselbring@outlook.com>
2017-06-18 09:52:12 -0500

    Added files and modified liri.ks

--- liri.js
+++ liri.js
@@ -1,2 +1,6 @@
-console.log('test');
-debugger;
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var request = require('request');
+var keys = require('./keys.js');
+
+console.log(keys.twitterKeys.consumer_key);
